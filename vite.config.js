974655bad4822cef1a724e6cb8d977556e29a599
nadin-c <nadinc.22it@kongu.edu>
2025-03-24 16:42:44 +0530

    fix: update output directory in Vite configuration from 'build' to 'dist' for consistency with Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // Ensure this matches the directory referenced in the Dockerfile
     sourcemap: true,
   },
   server: {
